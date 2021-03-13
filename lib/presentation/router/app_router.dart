66d511145a8a95e8b7f5fa7986c4296ec261d711
Ismail Fiki <smailfiki0808@gmail.com>
2021-03-13 11:22:31 +0100

    generated route

---
+++ lib/presentation/router/app_router.dart
@@ -0,0 +1,34 @@
+import 'package:bloc_pattern/presentation/screens/home_screen.dart';
+import 'package:bloc_pattern/presentation/screens/second_screen.dart';
+import 'package:bloc_pattern/presentation/screens/third_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route onGenerateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case '/':
+        return MaterialPageRoute(
+            builder: (_) => HomeScreen(
+                  title: 'Home Screen',
+                  color: Colors.blueAccent,
+                ));
+        break;
+      case '/second':
+        return MaterialPageRoute(
+            builder: (_) => SecondScreen(
+                  title: 'Second Screen',
+                  color: Colors.redAccent,
+                ));
+        break;
+      case '/third':
+        return MaterialPageRoute(
+            builder: (_) => ThirdScreen(
+                  title: 'Third Screen',
+                  color: Colors.greenAccent,
+                ));
+        break;
+      default:
+        return null;
+    }
+  }
+}
